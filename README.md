a9e90c56af53372b52648ae579bfadbd0ce1804d
Steffen Weinreich <lairsdragon@gmail.com>
2013-04-28 15:53:57 +0200

    Moved data dir to /var/lib/logstash changed the startup script to reflect this choosen defaults for my setup

--- README.md
+++ README.md
@@ -16,5 +16,15 @@ file. Alternatively, you can do it manually:
 
 4) run the following command:
 
-    fpm -n logstash -v 1.1.10 -a all -C logstash-packaging -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
+    fpm -n logstash -v 1.1.10 \
+        -a all \
+	-C logstash-packaging \
+        -m "<lesspublic@gmail.com>" \
+	--pre-install logstash-packaging/logstash.preinstall \
+	--description "Logstash Open Source Log Management" \
+	--url 'http://www.logstash.net/' 
+	-t deb \
+	--config-files etc/logstash/syslog.conf \
+	--config-files etc/default/logstash \
+	-s dir etc usr var
 
--- package.sh
+++ package.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
+
 VERSION=1.1.10
-cd ..
+RELEASE=2
+
+
 #git clone git://github.com/Yuav/logstash-packaging.git --depth=1
 # wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O logstash-packaging/usr/share/logstash/logstash.jar
-wget https://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar -O logstash-packaging/usr/share/logstash/logstash.jar
 
-fpm -n logstash -v $VERSION -a all -C logstash-packaging -m "Steve Weinreich <steve@weinreich.org>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
+if [ ! -f usr/share/logstash/logstash.jar ] ; then 
+    wget https://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar 
+         -O usr/share/logstash/logstash.jar
+fi
+
+fpm -n logstash \
+    -v "$VERSION-$RELEASE" \
+    -a all \
+    -m "Steve Weinreich <steve@weinreich.org>" \
+    --pre-install logstash.preinstall \
+    --description "Logstash Open Source Log Management" \
+    --url 'http://www.logstash.net/' \
+    -t deb \
+    --deb-user root \
+    --deb-group root \
+    --config-files etc/logstash/syslog.conf \
+    --config-files etc/default/logstash \
+    --exclude usr/share/logstash/.gitignore \
+    --exclude var/log/README \
+    -s dir etc usr var
