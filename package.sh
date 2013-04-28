ceebb2f0df36c867c141f828b1c6c6c87e1438f8
Steve Weinreich <lairsdragon@gmail.com>
2013-04-28 12:39:16 +0200

    Moved Home of Logstash  user to /var Use the flatjar logstash version

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@
 VERSION=1.1.10
 cd ..
 #git clone git://github.com/Yuav/logstash-packaging.git --depth=1
-wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O logstash-packaging/usr/share/logstash/logstash.jar
-#wget https://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar
-fpm -n logstash -v $VERSION -a all -C logstash-packaging -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
+# wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O logstash-packaging/usr/share/logstash/logstash.jar
+wget https://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar -O logstash-packaging/usr/share/logstash/logstash.jar
+
+fpm -n logstash -v $VERSION -a all -C logstash-packaging -m "Steve Weinreich <steve@weinreich.org>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
